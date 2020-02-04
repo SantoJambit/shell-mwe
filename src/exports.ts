1d389fcadcc633817a400ea9c8cfb2476f0e5534
Santo Pfingsten <santo.pfingsten@jambit.com>
2020-02-04 14:15:46 +0100

    cleanup

--- src/exports.ts
+++ src/exports.ts
@@ -1,4 +1,5 @@
 // This file should contain all exports we want to give to the pilet.
 import './extendApi';
-// export type Woot = string;
-export type Woot = 'hello' | 'world';
+
+export type Fail1 = string;
+export type Fail2 = 'hello' | 'world';
